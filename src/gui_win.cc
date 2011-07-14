c0ad3e7e3f047aac66080cd5c4959353dbf4e5ab
Carlos Pizano-Uribe <carlos.pizano@gmail.com>
2011-07-13 22:43:22 -0700

    Added the manifest dependency for the common controls v6, so the dialog looks modern. Note: I think the change in the vcxproj is irrelevant, but it does not hurt.

--- src/gui_win.cc
+++ src/gui_win.cc
@@ -9,6 +9,8 @@
 #include <string>
 #include <algorithm>
 
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 typedef std::vector<std::wstring> VoWStr;
 typedef std::vector<VoWStr*> VoVoWS;
 
